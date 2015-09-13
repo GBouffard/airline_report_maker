a80e48fb7b8af85717657d821732f015e98c0785
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-09-13 19:44:51 +0100

    RM can only process a valid input file

--- lib/report_maker.rb
+++ lib/report_maker.rb
@@ -2,5 +2,11 @@ class ReportMaker
   from_file, to_file = ARGV
 
   def initialize(from_file, to_file)
+    @flight_data = open(from_file).read
+    file_validation
+  end
+
+  def file_validation
+    fail 'This file is not valid' unless @flight_data.include?('route' && 'aircraft')
   end
 end
--- spec/report_maker_spec.rb
+++ spec/report_maker_spec.rb
@@ -1,11 +1,18 @@
 require 'report_maker'
 
 describe ReportMaker do
-  it 'requires 2 files to be given to work' do
-    expect { ReportMaker.new } .to raise_error
-  end
+  describe 'checks files validation' do
+    it 'requires 2 files to be given to work' do
+      expect { ReportMaker.new } .to raise_error
+    end
 
-  xit 'can only process a valid input file, meaning with 1 route and 1 aircraft' do
+    it 'can only process a valid input file, meaning with 1 route and 1 aircraft' do
+      # for simplicity reasons, we will assume a file is not valid when it does not include a route
+      # and an aircraft. There are many more tests that would need to be done to check the accuracy
+      # of each line, with arguments counts and nature of each argument (numeric/boolean/string).
+      invalid = 'not_a_valid_flight_file.txt'
+      expect { ReportMaker.new(invalid, 'file2') } .to raise_error('This file is not valid')
+    end
   end
 
   xit 'knows and calculates the total number of passengers' do
