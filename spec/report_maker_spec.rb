a535a9247f0cc059eb0a0be799c3b0568a1fbc99
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-09-13 16:09:38 +0100

    Written tests, bypassed for now

--- spec/report_maker_spec.rb
+++ spec/report_maker_spec.rb
@@ -1,4 +1,39 @@
 require 'report_maker'
 
 describe ReportMaker do
+  xit 'requires 2 files to be given to work' do
+  end
+
+  xit 'can only process a valid input file, meaning with 1 route and 1 aircraft' do
+  end
+
+  xit 'knows and calculates the total number of passengers' do
+  end
+
+  xit 'knows and calculates the number of general passengers' do
+  end
+
+  xit 'knows and calculates the number of airline passengers' do
+  end
+
+  xit 'knows and calculates the number of loyalty passengers' do
+  end
+
+  xit 'knows and calculates the total of loyalty points redeemded' do
+  end
+
+  xit 'knows and calculates the total cost of a flight' do
+  end
+
+  xit 'knows and calculates the total unadjusted tickets revenue' do
+  end
+
+  xit 'knows and calculates the total adjusted revenue' do
+  end
+
+  xit 'knows and informs if a flight can proceed' do
+  end
+
+  xit 'writes what is expected as the report in the output file' do
+  end
 end
